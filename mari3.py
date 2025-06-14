633cbbc97b801d1ce8a6778e185a12d8d83ad476
Lumy <hiwoko@protonmail.com>
2025-06-14 10:16:34 +0200

    initial commit

--- mari3.py
+++ mari3.py
@@ -1,7 +1,8 @@
 import os
 import discord
 from discord.ext import commands
-
+counter_ban_up = -1
+counter_ban_down = -1
 with open('/home/hiwo/discord_token', 'r') as data:
     token = data.read().strip()
 
@@ -13,10 +14,16 @@ bot = commands.Bot(command_prefix='$', intents=intents)
 @bot.event
 async def on_ready():
     print(f'Logged in as {bot.user}!')
-
-@bot.command()
-async def test(ctx, arg):
-    await ctx.send(arg)
+#@bot.event 
+#async def on_reaction_rem(reaction_rem, user):
+#       
+@bot.event
+async def on_reaction_add(reaction, user):
+    counter_up = 1
+    counter_down = 1
+    print(f"{reaction} from {user}")
+    print(int(reaction))
+    pass
 
 @bot.command()
 async def poll(ctx, user_id,*, question):
@@ -24,14 +31,10 @@ async def poll(ctx, user_id,*, question):
     message = await ctx.send(f"{question} - {user_id}: \n✅ = Yes\n❎ = No")
     await message.add_reaction('✅')
     await message.add_reaction('❎')
-
-@client.event
-async def on_reaction(reaction,user):
-  print(reaction)
+    
 
 @bot.command()
 async def you(ctx):
-    await ctx.send("fucker")  # Fixed missing await
-    print("it worked")
+    await ctx.send("fucker")
 
 bot.run(token)
