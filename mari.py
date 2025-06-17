c797ba5e616803a3d4a165c51be904fcd540459a
Lumy <hiwoko@protonmail.com>
2025-06-17 19:35:48 +0200

    timeouting

--- mari.py
+++ mari.py
@@ -2,6 +2,7 @@ import os
 import discord
 from discord.ext import commands
 import asyncio
+import datetime
 global total_member_count
 
 with open('/home/hiwo/discord_token', 'r') as data:
@@ -17,8 +18,9 @@ async def on_ready():
     print(f'Logged in as {bot.user}!')
 
 @bot.command()
-async def ban(ctx, user_id, *, question):
-    mess = await ctx.send(f"{question} - {user_id}: \n✅ = Yes\n❎ = No")
+async def time(ctx, member: discord.Member):
+
+    mess = await ctx.send(f"- {member}: \n✅ = Yes\n❎ = No")
     await mess.add_reaction('✅')
     await mess.add_reaction('❎')
     await asyncio.sleep(5)
@@ -27,13 +29,15 @@ async def ban(ctx, user_id, *, question):
 
     msg = await ctx.channel.fetch_message(mess.id) # 'Cache' the message
     highest_reaction = ""
-    highest_reaction_number = 1
+    highest_reaction_number = 3
     
     for reaction in msg.reactions: # iterate through every reaction in the message
-        if (reaction.count-3) >= highest_reaction_number:
+        if (reaction.count-1) >= highest_reaction_number:
             highest_reaction = reaction.emoji
             highest_reaction_count = reaction.count-1
-            await ctx.send(f"the person would be banned now")
+            await ctx.send(f"timeouted {member}!")
+            await member.timeout(datetime.timedelta(days=28))
+            #await interaction.response.send_message(f'was timeouted until for {duration}', ephemeral=True)
 
 
 @bot.command()
